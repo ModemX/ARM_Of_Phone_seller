d9bc4f8f9d880673c603a37bda81817255a7efef
Sad__Mod <maksimuk.irill@gmail.com>
2019-11-08 12:29:36 +0300

    Начало кода

---
+++ ARM_Of_Phone_seller_PROJECT/ARM_Of_Phone_seller_PROJECT/Auth.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ARM_Of_Phone_seller_PROJECT
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public MainWindow mainWindow;
+        public Auth(MainWindow _mainWindow)
+        {
+            InitializeComponent();
+
+            mainWindow = _mainWindow;
+        }
+
+        private void Auth_Button_Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Auth_Button_Guest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ARM_Of_Phone_seller_PROJECT/ARM_Of_Phone_seller_PROJECT/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ARM_Of_Phone_seller_PROJECT
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenPage(pages.login);
+        }
+
+        public enum pages
+        {
+            login,
+            info
+        }
+
+        void OpenPage(pages pages)
+        {
+            if (pages == pages.login)
+            {
+                MainFrame.Navigate(new Auth(this));
+            }
+            //else if(pages == pages.info)
+            //{
+            //    MainFrame.Navigate(new /*TODO*/(this));
+            //}
+        }
+
+
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("Existing_Users");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("");//Строка подключения
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            return dataTable;
+        }
+    }
+}
